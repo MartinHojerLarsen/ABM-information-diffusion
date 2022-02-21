e37a503130c320b2519f616522ff746a57e65370
Martin Højer Larsen <martinhojerlarsen@gmail.com>
2022-02-21 15:06:47 +0100

    FEAT, REFACTOR - test of json + request, added favicon

--- Website/flask_test.py
+++ Website/flask_test.py
@@ -1,26 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from markupsafe import escape
 
 # Commands to run server: 
-# $env:FLASK_APP = "hello"
+# $env:FLASK_APP = "flask_test"
 # flask run
 # site runs on http://127.0.0.1:5000/ 
 
 app = Flask(__name__)
 
+# debug on - updates changes to .py files automatically
 app.run(debug=True)
+# updates changes to templates (and static files) on refresh
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
-
+# main 
 @app.route("/")
 def index():
     return render_template('index.html')
 
 
+#json test
+@app.route("/data")
+def json_test(): 
+    return jsonify(
+        name = "Mads",
+        age = 42
+    )
+
+
+# test for sub URL - access on http://127.0.0.1:5000/test
 @app.route("/test")
 def test():
     return "<p>test</p>"
-
-
 
 
--- Website/static/canvas1.js
+++ Website/static/canvas1.js
@@ -1,20 +1,22 @@
 // Canvas 1 code 
 
-// Grid
-var canvas = document.getElementById("canvas1");
-var context = canvas.getContext("2d");
-
-// horisontal lines
-for (var x = 0.5; x < 401; x += 10) {
-    context.moveTo(x, 0);
-    context.lineTo(x, 400);
-}
-
-// vertical lines
-for (var y = 0.5; y < 401; y += 10) {
-    context.moveTo(0, y);
-    context.lineTo(400, y);
-}
-
-context.strokeStyle = "darkgray";
-context.stroke();
+function render_canvas1() {
+    // Grid
+    let canvas = document.getElementById("canvas1");
+    let context = canvas.getContext("2d");
+    
+    // horisontal lines
+    for (let x = 0.5; x < 401; x += 10) {
+        context.moveTo(x, 0);
+        context.lineTo(x, 400);
+    }
+    
+    // vertical lines
+    for (let y = 0.5; y < 401; y += 10) {
+        context.moveTo(0, y);
+        context.lineTo(400, y);
+    }
+    
+    context.strokeStyle = "darkgray";
+    context.stroke();
+};
