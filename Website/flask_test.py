6495f4f4ae3d764b339cd432766701d691c5fd18
Martin Højer Larsen <martinhojerlarsen@gmail.com>
2022-02-25 14:27:26 +0100

    FEAT - POST/GET, Canvas stuff

--- Website/flask_test.py
+++ Website/flask_test.py
@@ -1,21 +1,28 @@
-from flask import Flask, render_template, jsonify
+from unittest import result
+from flask import Flask, render_template, jsonify, request
 from markupsafe import escape
 
 # Commands to run server: 
 # $env:FLASK_APP = "flask_test"
+# $env:FLASK_ENV = "development"    - debug on, updates changes to .py files automatically
 # flask run
 # site runs on http://127.0.0.1:5000/ 
 
 app = Flask(__name__)
 
-# debug on - updates changes to .py files automatically
-app.run(debug=True)
 # updates changes to templates (and static files) on refresh
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 # main 
-@app.route("/")
+@app.route("/", methods=["POST", "GET"])
 def index():
+    if request.method == "POST": 
+        a = request.form.get("a")
+        b = request.form.get("b")
+        sumAB = str(int(a) + int(b))
+        print(sumAB)
+        return render_template('index.html', sumAB = sumAB)
+
     return render_template('index.html')
 
 
