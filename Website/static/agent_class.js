c1303182ef95755146090fffc774c289b3408c1d
Mads Petersen <mads.m.petersen@gmail.com>
2022-03-23 16:36:26 +0100

    Feat - Added object interaction and cursor identification

--- Website/static/agent_class.js
+++ Website/static/agent_class.js
@@ -1,146 +1,199 @@
-import { drawImageRot } from "/static/canvas_main.js";
+import { drawImageRot, draw_text } from "/static/canvas_main.js";
 
 // agent images
-let commoner_img_face1 = document.getElementById('commonerFace1');
-let rinfluencer_img_face1 = document.getElementById('rinfluencerFace1');
-let finfluencer_img_face1 = document.getElementById('finfluencerFace1');
+let commoner_img_face1 = document.getElementById("commonerFace1");
+let rinfluencer_img_face1 = document.getElementById("rinfluencerFace1");
+let finfluencer_img_face1 = document.getElementById("finfluencerFace1");
 
 class Agent {
-    constructor(ctx,ID, type, opinion, connections, x, y,size,rotation) {
-        this.ctx = ctx;
-        this.ID = ID;
-        this.type = type;
-        this.opinion = opinion;
-        this.connections = connections;
-        this.x = x;
-        this.y = y;
-        this.size = size;
-        this.rotation = rotation;
-    }
+  constructor(ctx, ID, type, opinion, connections, x, y, size, rotation) {
+    this.ctx = ctx;
+    this.ID = ID;
+    this.type = type;
+    this.opinion = opinion;
+    this.connections = connections;
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.rotation = rotation;
 
-    // Getters 
-    getID() {
-        return this.ID;
-    }
+    this.speechBubble = false;
+  }
 
-    getType() {
-        return this.type;
-    }
+  // Getters
+  getID() {
+    return this.ID;
+  }
 
-    getOpinion() {
-        return this.opinion;
-    }
+  getType() {
+    return this.type;
+  }
 
-    getConnections() {
-        return this.connections;
-    }
+  getOpinion() {
+    return this.opinion;
+  }
 
-    getX() {
-        return this.x;
-    }
+  getConnections() {
+    return this.connections;
+  }
 
-    getY() {
-        return this.y;
-    }
+  getX() {
+    return this.x;
+  }
 
-    getSize() {
-        return this.size;
-    }
-    getRotation() {
-        return this.rotation;
-    }
+  getY() {
+    return this.y;
+  }
 
-    setRotation(newRotation) {
-        this.rotation = newRotation;
-    }
+  getSize() {
+    return this.size;
+  }
+  getRotation() {
+    return this.rotation;
+  }
+
+  getSpeechBubble() {
+      this.speechBubble
+  }
+
+  setSpeechBubble(bool) {
+    this.speechBubble = bool;
+}
+
+  setRotation(newRotation) {
+    this.rotation = newRotation;
+  }
+
 }
 
 class Commoner extends Agent {
-    constructor(ctx,ID, type, opinion, connections, x, y,size,rotation = 0) {
-        super(ctx,ID, type, opinion, connections, x, y,size,rotation);
+  constructor(ctx, ID, type, opinion, connections, x, y, size, rotation = 0,speechBubble) {
+    super(ctx, ID, type, opinion, connections, x, y, size, rotation,speechBubble);
 
-        this.y_upperB = y - 15
-        this.y_lowerB = y + 15
-        this.speed = 0.25
-        this.dy = this.speed;
+    this.y_upperB = y - 15;
+    this.y_lowerB = y + 15;
+    this.speed = 0.25;
+    this.dy = this.speed;
+  }
+
+  draw() {
+    //To be deleted
+    this.ctx.fillStyle = "lightgreen";
+    this.ctx.fillRect(
+      this.x - this.size / 2,
+      this.y - this.size / 2,
+      this.size,
+      this.size
+    );
+    drawImageRot(
+      this.ctx,
+      commoner_img_face1,
+      this.x,
+      this.y,
+      this.size,
+      this.size,
+      this.rotation
+    );
+  }
+
+  minorHover() {
+    this.y += this.dy;
+    if (this.y > this.y_upperB) this.dy -= this.speed;
+    if (this.y < this.y_lowerB) this.dy += this.speed;
+
+    if (this.speechBubble) {
+      draw_text(this.ctx, "I am a Commoner", this.x, this.y - 150);
     }
 
-    draw() {
-        drawImageRot(this.ctx,commoner_img_face1,this.x,this.y,this.size,this.size,this.rotation); 
-    }
-
-    minorHover() {
-        this.y += this.dy
-        if(this.y > this.y_upperB) this.dy -= this.speed;
-        if(this.y < this.y_lowerB) this.dy += this.speed;
-    }
-
-    update() {
-        // Animation elements
-        this.minorHover();
-
-        // Draw animation
-        this.draw()
-    }
+    this.draw();
+  }
 }
 
 class R_influencer extends Agent {
-    constructor(ctx,ID, type, opinion, connections, x, y,size,rotation) {
-        super(ctx,ID, type, opinion, connections, x, y,size,rotation);
-        
-        this.y_upperB = y - 15
-        this.y_lowerB = y + 15
-        this.speed = 0.25
-        this.dy = this.speed;
+  constructor(ctx, ID, type, opinion, connections, x, y, size, rotation,speechBubble) {
+    super(ctx, ID, type, opinion, connections, x, y, size, rotation,speechBubble);
+
+    this.y_upperB = y - 15;
+    this.y_lowerB = y + 15;
+    this.speed = 0.29;
+    this.dy = this.speed;
+  }
+
+  draw() {
+    //To be deleted
+    this.ctx.fillStyle = "lightgreen";
+    this.ctx.fillRect(
+      this.x - this.size / 2,
+      this.y - this.size / 2,
+      this.size,
+      this.size
+    );
+
+    drawImageRot(
+      this.ctx,
+      rinfluencer_img_face1,
+      this.x,
+      this.y,
+      this.size,
+      this.size,
+      this.rotation
+    );
+  }
+
+  minorHover() {
+    this.y += this.dy;
+    if (this.y > this.y_upperB) this.dy -= this.speed;
+    if (this.y < this.y_lowerB) this.dy += this.speed;
+
+    if (this.speechBubble) {
+      draw_text(this.ctx, "I am an Influencer", this.x, this.y - 150);
     }
 
-    draw() {
-        drawImageRot(this.ctx,rinfluencer_img_face1,this.x,this.y,this.size,this.size,this.rotation); 
-    };
-
-    minorHover() {
-        this.y += this.dy
-        if(this.y > this.y_upperB) this.dy -= this.speed;
-        if(this.y < this.y_lowerB) this.dy += this.speed;
-    }
-
-    update() {
-        // Animation elements
-        this.minorHover();
-
-        // Draw animation
-        this.draw()
-    }
-
+    this.draw();
+  }
 }
 
 class F_influencer extends Agent {
-    constructor(ctx,ID, type, opinion, connections, x, y,size,rotation) {
-        super(ctx,ID, type, opinion, connections, x, y,size,rotation);
-    
-        this.y_upperB = y - 15
-        this.y_lowerB = y + 15
-        this.speed = 0.25
-        this.dy = this.speed;
+  constructor(ctx, ID, type, opinion, connections, x, y, size, rotation,speechBubble) {
+    super(ctx, ID, type, opinion, connections, x, y, size, rotation,speechBubble);
+
+    this.y_upperB = y - 15;
+    this.y_lowerB = y + 15;
+    this.speed = 0.31;
+    this.dy = this.speed;
+  }
+
+  draw() {
+    //To be deleted
+    this.ctx.fillStyle = "lightgreen";
+    this.ctx.fillRect(
+      this.x - this.size / 2,
+      this.y - this.size / 2,
+      this.size,
+      this.size
+    );
+    drawImageRot(
+      this.ctx,
+      finfluencer_img_face1,
+      this.x,
+      this.y,
+      this.size,
+      this.size,
+      this.rotation
+    );
+  }
+
+  minorHover() {
+    this.y += this.dy;
+    if (this.y > this.y_upperB) this.dy -= this.speed;
+    if (this.y < this.y_lowerB) this.dy += this.speed;
+
+    if (this.speechBubble) {
+      draw_text(this.ctx, "I am an Influencer", this.x, this.y - 150);
     }
 
-    draw() {
-        drawImageRot(this.ctx,finfluencer_img_face1,this.x,this.y,this.size,this.size,this.rotation); 
-    };
-
-    minorHover() {
-        this.y += this.dy
-        if(this.y > this.y_upperB) this.dy -= this.speed;
-        if(this.y < this.y_lowerB) this.dy += this.speed;
-    }
-
-    update() {
-        // Animation elements
-        this.minorHover();
-
-        // Draw animation
-        this.draw()
-    }
+    this.draw();
+  }
 }
 
-export {Commoner,R_influencer,F_influencer};
+export { Commoner, R_influencer, F_influencer };
--- Website/static/canvas_main.js
+++ Website/static/canvas_main.js
@@ -3,14 +3,14 @@
 // let commoner_img = document.getElementById('commoner_img');
 
 // Background
-function background(canvas, ctx, color="#eeeaea") {
-    
+function background(canvas, ctx, width, height, color = "#eeeaea") {
+
     // Size
-    ctx.canvas.width = window.innerWidth;
-    ctx.canvas.height = window.innerHeight;
-    
+    ctx.canvas.width = width;
+    ctx.canvas.height = height;
+
     // Border and background
-    ctx.moveTo(0,0);
+    ctx.moveTo(0, 0);
     ctx.beginPath();
     ctx.rect(2, 2, canvas.width - 3, canvas.height - 3);
     ctx.fillStyle = color;
@@ -19,8 +19,8 @@ function background(canvas, ctx, color="#eeeaea") {
 };
 
 // Clear canvas
-function clear_canvas(canvas, ctx, color="#eeeaea") {
-    ctx.moveTo(0,0);
+function clear_canvas(canvas, ctx, color = "#eeeaea") {
+    ctx.moveTo(0, 0);
     ctx.beginPath();
     ctx.rect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = color;
@@ -29,8 +29,20 @@ function clear_canvas(canvas, ctx, color="#eeeaea") {
     ctx.closePath();
 };
 
+// Draw text on
+function draw_text(ctx, text, x, y, txt_size = 30) {
+
+    ctx.fillStyle = 'lightblue';
+    ctx.fillRect(x - 125, y - 30, 250, 75)
+
+    ctx.font = `italic ${txt_size}px Calibri`;
+    ctx.strokeStyle = "black";
+    ctx.textAlign = "center";
+    ctx.strokeText(text, x, y + 20)
+}
+
 // Offset image x,y coordinates to center of image
-function drawImageRot(ctx,img,x,y,width,height,deg) {
+function drawImageRot(ctx, img, x, y, width, height, deg) {
     //Convert degrees to radian
     var rad = deg * Math.PI / 180;
     //Set the origin to the center of the image
@@ -40,17 +52,17 @@ function drawImageRot(ctx,img,x,y,width,height,deg) {
     //draw the image
     ctx.drawImage(img, width / 2 * (-1), height / 2 * (-1), width, height);
     //reset the canvas
-    ctx.rotate(rad * ( -1 ) );
+    ctx.rotate(rad * (-1));
     //
     ctx.translate((x) * (-1), (y) * (-1));
-  }
+}
 
 // Line (edge) 
-function line(ctx, x1, y1, x2, y2, size=1) {
+function line(ctx, x1, y1, x2, y2, size = 1) {
     ctx.strokeStyle = "black";
     ctx.lineWidth = size;
     ctx.beginPath();
-    ctx.moveTo(x1,y1);
+    ctx.moveTo(x1, y1);
     ctx.lineTo(x2, y2);
     ctx.stroke();
     ctx.closePath();
@@ -58,47 +70,26 @@ function line(ctx, x1, y1, x2, y2, size=1) {
 
 // Mouse hover handling
 function hover(e, object) {
-    e.preventDefault(); 
+    e.preventDefault();
     e.stopPropagation();
-    
+
     // Cursor position
     let cx = e.offsetX;
     let cy = e.offsetY;
     console.log(cx, cy);
-    
-    let isInside = touchingCircle(object, cx, cy); 
+
+    let isInside = touchingCircle(object, cx, cy);
     // console.log(isInside);
-    
+
     if (isInside) {
         document.body.style.cursor = "pointer";
     } else {
         document.body.style.cursor = "default";
     }
-    
+
 }
 
 //*** Helper functions ***//
-
-// detect circle
-// if(Math.pow(x-50,2)+Math.pow(y-50,2) < Math.pow(50,2))  
-function touchingCircle(ob, x, y) {
-    return Math.sqrt((x-ob.x) ** 2 + (y - ob.y) ** 2) < ob.size;
-};
-
-// detect square
-function touchingSquare(ob, x, y) {
-    let obX = ob.x;
-    let obY = ob.y;
-    let size = ob.size;
-    
-    // Returning true/false if (x,y) (mouse coords) are inside the object 
-    return (x >= obX-size && x <= obX+size && y >= obY-size && y <= obY+size);
-};
-
-// detect triangle
-function touchinTriangle(ob, x, y) {
-    // to be continued
-};
 
 // Random number for drawing agents - used for positions
 function randInt(min, max) {
@@ -128,6 +119,35 @@ function color_agent(type, opinion) {
     }
 };
 
+function getCursorPosition(canvas, event) {
+    const rect = canvas.getBoundingClientRect()
+    const mouseX = event.clientX - rect.left
+    const mouseY = event.clientY - rect.top
+    return { 'x': mouseX, 'y': mouseY }
+}
+
+function insideObj(obj, mousePos) {
+    if (mousePos.x > obj.getX() - obj.getSize() / 2            // mouse x between x and x + width
+        && mousePos.x < obj.getX() + obj.getSize() / 2
+        && mousePos.y > obj.getY() - obj.getSize() / 2            // mouse y between y and y + height
+        && mousePos.y < obj.getY() + obj.getSize() / 2) {
+        return true
+    }
+    return false
+}
+
+function change_cursor(objArray,mousePos) {
+    let checkList = [];
+    for (let index = 0; index < objArray.length; index++) {
+        checkList[index] = insideObj(objArray[index],mousePos);
+    }
+    if(checkList.includes(true)) {
+        document.body.style.cursor = 'pointer';
+    } else {
+        document.body.style.cursor = 'default';
+    }
+}
+
 
 // Export
-export {background, clear_canvas, randInt, line, hover,drawImageRot};
+export { background, clear_canvas, randInt, line, hover, drawImageRot, draw_text, getCursorPosition, insideObj,change_cursor};
